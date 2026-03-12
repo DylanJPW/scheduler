4e134ee2ba82f6fa168b4a5bedcf354d0ddc3884
Dylan Winters <djpwinters2002@gmail.com>
2026-03-12 11:18:22 +0000

    Initial creation of app

---
+++ scheduler-backend/src/test/java/com/scheduler/scheduler_backend/SchedulerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheduler.scheduler_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
